5aa89becafe674a0664b4886ed85ed701d7846bb
TaricPinkFriend <taricpinkfriend@gmail.com>
2023-03-07 18:40:38 +0800

    test other mathjax configs

--- website/book/config_mathjax_fonts.js
+++ website/book/config_mathjax_fonts.js
@@ -1,3 +1,18 @@
 MathJax.Hub.Config({
-    "HTML-CSS": { fonts: ["STIX", "Neo-Euler", "Gyre-Termes", "Gyre-Pagella"]}
+    jax: ["input/TeX", "input/MathML",
+        "output/SVG", "output/PreviewHTML"
+    ],
+    "HTML-CSS": {
+        imageFont: null,
+        webFont: "STIX-Web",
+        availableFonts: [],
+        preferredFont: null,
+        // fonts: ["STIX", "Neo-Euler", "Gyre-Termes", "Gyre-Pagella", "Asana-Math"],
+        undefinedFamily: "STIXGeneral, 'Arial Unicode MS', serif",
+        linebreaks: {automatic: true},
+        showMathMenu: true
+    },
+    "SVG": {
+        font: "STIX-Web",
+    }
 });
