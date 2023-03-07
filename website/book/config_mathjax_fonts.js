f5478e132e8bde389045b8fe389a65a7ff3d6f63
TaricPinkFriend <taricpinkfriend@gmail.com>
2023-03-07 17:00:49 +0800

    configured mathjax further
    enable rightclicks on formulas, finetune fonts

--- website/book/config_mathjax_fonts.js
+++ website/book/config_mathjax_fonts.js
@@ -1,3 +1,8 @@
 MathJax.Hub.Config({
-    "HTML-CSS": { fonts: ["STIX", "Neo-Euler", "Gyre-Termes", "Gyre-Pagella"]}
+    "HTML-CSS": {
+        fonts: ["STIX", "Neo-Euler", "Gyre-Termes", "Gyre-Pagella", "Asana-Math"],
+        undefinedFamily: "STIXGeneral, 'Arial Unicode MS', serif",
+        linebreaks: {automatic: true},
+        showMathMenu: true
+    }
 });
